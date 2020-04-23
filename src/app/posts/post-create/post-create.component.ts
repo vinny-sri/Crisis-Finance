2c8851a70f9b9a57c7524101407959132d9004d0
vinny-sri <vinny.sri@berkeley.edu>
2020-04-23 05:35:09 -0700

    Created post input option that displays inputted text

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+})
+export class PostCreateComponent {
+  newPost = "NO CONTENT";
+  onAddPost(postInput: HTMLTextAreaElement) {
+    this.newPost = postInput.value;
+  }
+}
